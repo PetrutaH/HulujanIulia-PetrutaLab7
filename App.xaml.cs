88aaf1c91908cfb68cd8224cd36eac00a837b259
PetrutaH <167861117+PetrutaH@users.noreply.github.com>
2024-11-27 19:25:44 +0200

    laborator8-gata

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,7 +1,26 @@
-﻿namespace HulujanIulia_PetrutaLab7
+﻿using System;
+using HulujanIulia_PetrutaLab7.Data;
+using System.IO;
+
+
+namespace HulujanIulia_PetrutaLab7
 {
     public partial class App : Application
     {
+        static ShoppingListDatabase database;
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new
+                   ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+                   LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
         public App()
         {
             InitializeComponent();
